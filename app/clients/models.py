18e595d91800aaa1a2ed9b567f82ee1028d25942
vladmspb <nevasv@yandex.ru>
2023-01-12 00:03:28 +0300

    models

--- app/clients/models.py
+++ app/clients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT) # связываю базовых User c классом и проверяю связи при удалении
+    company_name = models.CharField(max_length=100)
+    telegram_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Client: {self.company_name}"
